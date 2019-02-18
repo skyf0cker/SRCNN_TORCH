76233c9b2303f97c349672695b62a1f93077c546
tenshine <tanwnx@outlook.com>
2019-02-18 09:45:12 +0800

    srcnn

---
+++ dataLoadess.py
@@ -0,0 +1,40 @@
+from torch.utils.data import Dataset
+import os
+from PIL import Image
+import numpy as np
+import torchvision.transforms as transform
+
+
+class Imgdataset(Dataset):
+
+    def __init__(self, path):
+        super(Imgdataset, self).__init__()
+        self.data = []
+        if os.path.exists(path):
+            dir_list = os.listdir(path)
+            LR_dir = dir_list[0]
+            HR_dir = dir_list[1]
+            LR_path = path + '/' + LR_dir
+            HR_path = path + '/' + HR_dir
+            if os.path.exists(LR_path) and os.path.exists(HR_path):
+                LR_data = os.listdir(LR_path)
+                HR_data = os.listdir(HR_path)
+                self.data = [{'LR':LR_path + '/' + LR_data[i], 'HR':HR_path + '/' + HR_data[i]} for i in range(len(LR_data))]
+            else:
+                raise FileNotFoundError('path doesnt exist!')
+
+    def __getitem__(self, index):
+
+        LR_path, HR_path = self.data[index]["LR"], self.data[index]["HR"]
+        tran = transform.ToTensor()
+
+        LR_img = Image.open(LR_path)
+        HR_img = Image.open(HR_path)
+        # print(tran(img).shape)
+
+        return tran(LR_img), tran(HR_img)
+
+    def __len__(self):
+
+        return len(self.data)
+
---
+++ train.py
@@ -0,0 +1,74 @@
+from dataLoadess import Imgdataset
+from torch.utils.data import DataLoader
+from models import NetWork
+import torch.optim as optim
+import torch.nn as nn
+import torch
+
+from torch.autograd import Variable
+
+if not torch.cuda.is_available():
+    raise Exception('NO GPU!')
+
+dataset = Imgdataset("/home/tenshine/Desktop/SRCNN/data")
+train_data_loader = DataLoader(dataset=dataset, num_workers=10, batch_size=20, shuffle=True)
+
+net = NetWork()
+loss = nn.MSELoss()
+net.cuda()
+loss.cuda()
+
+optimizer = optim.Adam(net.parameters(), lr=0.01)
+
+def train(epoch):
+    epoch_loss = 0
+    for iteration,  batch in enumerate(train_data_loader):
+        input, target = Variable(batch[0]), Variable(batch[1])
+        input = input.cuda()
+        target = target.cuda()
+        target = target[:, :, 6:26, 6:26]
+        optimizer.zero_grad()
+        # print ("input shape = " , input.shape)
+        # print ("target shape = ", target.shape)
+        model_out = net(input)
+
+        # print ("model_out shape =" , model_out.shape)
+        Loss = loss(model_out, target)
+        # print('Loss data', Loss.data)
+        epoch_loss += Loss.data
+        Loss.backward()
+        optimizer.step()
+
+        print("===> Epoch[{}]({}/{}): Loss: {:.4f}".format(epoch, iteration, len(train_data_loader), Loss.data))
+    print("===> Epoch {} Complete: Avg. Loss: {:.4f}".format(epoch, epoch_loss / len(train_data_loader)))
+
+
+def test():
+    avg_psnr = 0
+    for batch in testing_data_loader:
+        input, target = Variable(batch[0]), Variable(batch[1])
+        if use_cuda:
+            input = input.cuda()
+            target = target.cuda()
+
+        prediction = srcnn(input)
+        
+    #     mse = criterion(prediction, target)
+    #     psnr = 10 * log10(1 / mse.data[0])
+    #     avg_psnr += psnr
+    # print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
+
+
+def checkpoint(epoch):
+    model_out_path = "model_epoch_{}.pth".format(epoch)
+    torch.save(net, model_out_path)
+    print("Checkpoint saved to {}".format(model_out_path))
+
+for epoch in range(1, 201):
+    print('epoch starts')
+    train(epoch)
+    # test()
+    if(epoch%10==0):
+        checkpoint(epoch)
+
+
